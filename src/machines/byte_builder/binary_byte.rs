d9bb7ed17ab1a5dc9341e0f93e7c19ab28f1e41c
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-03 23:50:00 -0700

    - change BinaryByte from type alias to struct. - change functions defined in this module to be part of BinaryByte impl block - impl Debug for BinaryByte to print the bit string

--- src/machines/byte_builder/binary_byte.rs
+++ src/machines/byte_builder/binary_byte.rs
@@ -2,93 +2,76 @@
 //! store its value. It provides functionality for converting binary bytes to integer bytes, and
 //! vice versa. It also provides a function to convert it into an easily readable bitstring.
 
-pub type BinaryByte = [bool; 8];
+use std::fmt::Debug;
 
-/// Convert the binary byte **in place** to be its negative equivalent.
-/// # Arguments
-/// - `bin`: The binary byte
-fn sign_change(bin: &mut BinaryByte) {
-    for bit in bin.iter_mut() {
-        *bit = !*bit;
-    }
-    for i in 7..0 {
-        bin[i] = !bin[i];
-        if !bin[i] {
-            break;
+pub struct BinaryByte {
+    bits: [bool; 8],
+}
+
+impl BinaryByte {
+    /// Calculate the opposite BinaryByte.
+    pub fn twos_comp(&self) -> Self {
+        let mut bits = self.bits.clone();
+        for bit in bits.iter_mut() {
+            *bit = !*bit;
         }
+        for i in 7..0 {
+            bits[i] = !bits[i];
+            if !bits[i] {
+                break;
+            }
+        }
+        Self { bits: bits }
+    }
+
+    pub fn to_bitstring(&self) -> String {
+        let mut bits = String::new();
+        for (i, bit) in self.bits.iter().enumerate() {
+            bits.push(match *bit {
+                true => '1',
+                false => '0',
+            });
+            if i == 3 {
+                bits.push(' ');
+            }
+        }
+        bits
+    }
+
+    pub fn value(&self) -> i8 {
+        let neg = self.bits[0];
+        let base: i8 = 2;
+
+        let bits = match neg {
+            true => self.twos_comp().bits,
+            false => self.bits,
+        };
+
+        let mut dec = 0;
+        for (i, bit) in bits.iter().enumerate() {
+            let value = (base as i16).pow(7 - i as u32) as i8;
+            if *bit {
+                dec += value;
+            }
+        }
+
+        match neg {
+            true => -1 - dec,
+            false => dec,
+        }
+    }
+
+    pub fn bits(&mut self) -> &mut [bool; 8] {
+        &mut self.bits
+    }
+
+    pub fn new() -> Self {
+        Self { bits: [false; 8] }
     }
 }
 
-/// Convert an integer into a binary byte.
-/// # Arguments
-/// - `dec`: The decimal number to convert
-/// # Returns
-/// The two's complement binary equivalent
-#[allow(dead_code)]
-pub fn to_binary(dec: i8) -> BinaryByte {
-    let mut remaining = dec.clone();
-    let base: i8 = 2;
-    let neg = remaining < 0;
-    remaining = (remaining as i16).abs() as i8;
-
-    let mut bin = [false; 8];
-    for (i, bit) in bin.iter_mut().enumerate() {
-        let value = base.pow(7 - i as u32);
-        if remaining - value >= 0 {
-            remaining -= value;
-            *bit = true;
-        }
+impl Debug for BinaryByte {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.to_bitstring())
     }
-
-    if neg {
-        sign_change(&mut bin);
-    }
-
-    bin
-}
-
-/// Convert a binary byte into an integer.
-/// # Arguments
-/// - `bin`: The binary byte
-/// # Returns
-/// The equivalent integer
-pub fn to_decimal(bin: &BinaryByte) -> i8 {
-    let mut clone = bin.clone();
-    let neg = clone[0];
-    let base: i8 = 2;
-    if neg {
-        sign_change(&mut clone);
-    }
-
-    let mut dec = 0;
-    for (i, bit) in clone.iter().enumerate() {
-        let value = (base as i16).pow(7 - i as u32) as i8;
-        if *bit {
-            dec += value;
-        }
-    }
-
-    match neg {
-        true => -1 - dec,
-        false => dec,
-    }
-}
-
-/// Convert a byte into a bitstring (usually for testing purposes).
-/// # Arguments
-/// - `bin` The binary byte
-/// # Returns
-/// The bitstring representing that byte
-pub fn to_bitstring(bin: &BinaryByte) -> String {
-    let mut bits = String::new();
-    for (i, bit) in bin.iter().enumerate() {
-        bits.push(match *bit {
-            true => '1',
-            false => '0',
-        });
-        if i == 3 {
-            bits.push(' ');
-        }
-    }
-    bits
 }
