3225b937f387b298d5ae75063e4117262c44bef6
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-03 18:14:25 -0700

    define the byte builder machine, make it implement SlotMachine. logic is untested.

--- src/machines/byte_builder.rs
+++ src/machines/byte_builder.rs
@@ -1,1 +1,40 @@
+use rand::{Rng, rng};
+
+use crate::machines::{
+    SlotMachine,
+    byte_builder::binary_byte::{BinaryByte, to_decimal},
+};
+
 mod binary_byte;
+
+pub struct ByteBuilder {
+    byte: BinaryByte,
+    multiplier: u32,
+}
+
+impl SlotMachine for ByteBuilder {
+    fn new() -> Self {
+        ByteBuilder {
+            byte: [false; 8],
+            multiplier: 1,
+        }
+    }
+
+    fn set_mult(&mut self, mult: u32) {
+        self.multiplier = match mult {
+            0 => 1,
+            _ => mult,
+        };
+    }
+
+    fn spin(&mut self) {
+        let mut rng = rng();
+        for bit in self.byte.iter_mut() {
+            *bit = rng.random_bool(0.5)
+        }
+    }
+
+    fn payout(&self) -> i128 {
+        to_decimal(&self.byte) as i128 * self.multiplier as i128
+    }
+}
