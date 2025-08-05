20a6621463b6958c1f4582d0a465bd4e38f231ce
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-04 17:10:22 -0700

    Make the router serve the handler for spinning byte builder.

--- src/router.rs
+++ src/router.rs
@@ -2,9 +2,11 @@
 //! handlers found in the `handlers` module. The documentation for the API can be found described in
 //! the `openapi.yaml` file in the root, or served with a GET request to the base path.
 
-use axum::Router;
+use axum::{Router, routing::post};
+
+use crate::handlers::byte_builder::handle_spin_byte_builder;
 
 /// Provide the HTTP router for the app **without** its required state.
 pub fn router() -> Router {
-    Router::new()
+    Router::new().route("/byte_builder", post(handle_spin_byte_builder))
 }
